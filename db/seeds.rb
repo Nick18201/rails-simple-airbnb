40ff0a9e339ba15f2e6790de9e988b56047894ee
nicolas blum <nblumferracci@gmail.com>
2020-11-18 16:48:48 +0100

    css to finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating 4 flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Spacious Garden London',
+  address: '12 Clifton Gardens',
+  description: 'A spacious garden flat. 1 double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Spacious Garden Paris',
+  address: '12 rue Gardens',
+  description: '2 double bedrooms, small kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Luxurious bedroom with balcoony',
+  address: '3 rue rubens',
+  description: 'open plan living, 1 small bedroom, no kitchen',
+  price_per_night: 1000,
+  number_of_guests: 1
+)
+
+puts 'finished'
